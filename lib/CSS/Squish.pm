289591471f0172fa9aefb341d2272f3ab1c3bf0b
Ruslan Zakirov <ruz@bestpractical.com>
2007-11-09 12:29:39 +0000

    bump version, update changes and manifest

--- lib/CSS/Squish.pm
+++ lib/CSS/Squish.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package CSS::Squish;
 
-$CSS::Squish::VERSION = '0.06';
+$CSS::Squish::VERSION = '0.07';
 
 # Setting this to true will enable lots of debug logging about what
 # CSS::Squish is doing
